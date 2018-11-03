a4f0ad2e6c79e978fde2bf9e314133746085ecd9
var_fix <moonnightnk@gmail.com>
2018-11-03 00:14:31 -0400

    functional:)

--- js.js
+++ js.js
@@ -1,3 +1,8 @@
+var balance = 0;
+var company = 500;
+var deposit = 0;
+
+
 var name = prompt("your account email: ")
 
 do {
@@ -6,3 +11,18 @@ do {
 } while(password != reenter);
 
 alert("Account created for: " + name);
+
+var choise = Number(prompt("pick an option: FOR DEPOSITS = 1: FOR TRANSFERS = 2: FOR BALANCE = 3 " ));
+if (choise === 3)
+	alert("Your balance is $2M.... just kidding: balance = "+ balance);
+else if (choise === 1)
+	alert("Your Working account has $" + company)
+	do {
+		var deposit = prompt("how much would you like to deposit from working account? ")
+	} while(deposit > company)
+	balance = balance + deposit;
+	alert("your balance now is "+ balance +"$");
+
+
+
+
