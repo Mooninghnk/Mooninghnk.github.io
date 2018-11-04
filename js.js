b8e37f8a106da44f0e0ce8144484dffad59e5648
var_fix <moonnightnk@gmail.com>
2018-11-04 00:36:16 -0400

    update

--- js.js
+++ js.js
@@ -1,28 +1,7 @@
-var balance = 0;
-var company = 500;
-var deposit = 0;
+function getRandomInt(max) {
+	return Math.floor(Math.random() * Math.floor(max));
+}
 
+let l =  Number(prompt("give me a number to ranzodmize"));
 
-var name = prompt("your account email: ")
-
-do {
-	var password = prompt("Create a Passowrd ");
-	var reenter = prompt("re_enter ");
-} while(password != reenter);
-
-alert("Account created for: " + name);
-
-var choise = Number(prompt("pick an option: FOR DEPOSITS = 1: FOR TRANSFERS = 2: FOR BALANCE = 3 " ));
-if (choise === 3)
-	alert("Your balance is $2M.... just kidding: balance = "+ balance);
-else if (choise === 1)
-	alert("Your Working account has $" + company)
-	do {
-		var deposit = prompt("how much would you like to deposit from working account? ")
-	} while(deposit > company)
-	balance = balance + deposit;
-	alert("your balance now is "+ balance +"$");
-
-
-
-
+alert(getRandomInt(l));
