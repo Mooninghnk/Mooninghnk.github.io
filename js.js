1b44aadc2404a4360833b1b7adb19a5ee3e31e0e
Mooninghnk <moonnightnk@gmail.com>
2018-12-15 13:16:39 -0500

    update_1.0

--- js.js
+++
@@ -1,7 +0,0 @@
-function getRandomInt(max) {
-	return Math.floor(Math.random() * Math.floor(max));
-}
-
-let l =  Number(prompt("give me a number to ranzodmize"));
-let b = 20;
-alert(getRandomInt(l));
