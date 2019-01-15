6457c68ca638d5454f25e2e9c09a72ed5d697a9e
TOOTO <Mooninghnk@gmail.com>
2019-01-15 01:35:52 -0500

    0.0.4

--- js.js
+++ js.js
@@ -1,16 +1,25 @@
 if(typeof web3 !== "undefined") {
     web3 = new Web3(web3.currentProvider);
     web3.eth.getAccounts(function (error, result) {
-        if (result[0] == 'undefined' || "null") {
-            alert("Login to your metamask");
-        }
         document.getElementById('address').innerHTML = "My Ethereum Wallet Is = " + result[0];
         let account1 = result[0];
         web3.eth.getBalance(account1, function(error, result) {
            let balance = result.c[0];
+           console.log(result);
            document.getElementById("balance").innerHTML = "My Balnce = " + balance;
         });
         ethereum.enable();
-    })};
-
-
+    })
+    function sendTo() {
+        web3.eth.getAccounts(function (error, result) {
+            let account = result[0];
+        web3.eth.sendTransaction({
+            from: account,
+            to: document.getElementById("toAdd").value,
+            value: document.getElementById("much").value * 1e18
+        }, function(error, hash){
+            console.log(hash);
+            });
+        })
+    }
+};
