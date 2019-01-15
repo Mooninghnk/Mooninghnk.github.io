a9d4d8d278c790de7dc306999a0bed11413e0470
TOOTO <Mooninghnk@gmail.com>
2019-01-15 16:43:16 -0500

    Background-newadds

--- js.js
+++ js.js
@@ -2,13 +2,12 @@ if (typeof web3 !== "undefined") {
   web3 = new Web3(web3.currentProvider);
       web3.eth.getAccounts(function(error, result) {
         document.getElementById("address").innerHTML =
-          "My Ethereum Wallet Is = " + result[0];
+          result[0];
         let account1 = result[0];
         web3.eth.getBalance(account1, function(error, result) {
           let balance = result.c[0];
-          document.getElementById("balance").innerHTML = "My Balnce = " + balance;
+          document.getElementById("balance").innerHTML = "Balance = " + balance;
         });
-        ethereum.enable();
         ethereum.on("accountsChanged", function(accounts) {
           window.location.reload();
         });
@@ -40,4 +39,9 @@ if (typeof web3 !== "undefined") {
           }, 5000);
         });
       });
+      function copyToClipboard_textArea() {
+        var copyText = document.getElementById("address");
+        copyText.select();
+        document.execCommand("Copy");
+      }
 }
