62026f317e27f70c909f9c847855783582200776
TOOTO <Mooninghnk@gmail.com>
2019-01-14 23:36:45 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,14 +1,16 @@
-
 if(typeof web3 !== "undefined") {
     web3 = new Web3(web3.currentProvider);
     web3.eth.getAccounts(function (error, result) {
+        if (result[0] == 'undefined' || "null") {
+            alert("Login to your metamask");
+        }
         document.getElementById('address').innerHTML = "My Ethereum Wallet Is = " + result[0];
         let account1 = result[0];
         web3.eth.getBalance(account1, function(error, result) {
            let balance = result.c[0];
            document.getElementById("balance").innerHTML = "My Balnce = " + balance;
         });
-        web3.eth.getPrivate
         ethereum.enable();
-    });
-}
+    })};
+
+
