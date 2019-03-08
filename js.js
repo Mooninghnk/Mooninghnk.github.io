ee21d0f566fb0e636f6b18646cbd4e11a243fc5a
Mooninghnk <moonnightnk@gmail.com>
2019-03-08 02:58:32 -0500

    menu added

--- js.js
+++ js.js
@@ -1,14 +1,26 @@
 //html page
 addMessage(body, "<center><h1 class='msg'></h1></center>");
+addMessage(body, `<button class="menu"><h1>---</h1></button>`)
 addMessage(body, "<div class='terminal'></div>");
 addMessage(body, "<br>", "");
 addMessage(body, "<center><h3 class='intro'></h3></center>");
 addMessage(body, `<center><div class="xeum-window" style ="display: none;"></div></center>`, "xeum");
+addMessage(body, `<center><button class="xcontent"><h1>👇</h1></button></center>`)
+addMessage(body, `
+<center>
+
+
+</center>
+
+`, "othct")
 
 
 
+
+const but = selector(".xcontent");
 
 let msgg = selector(".msg");
+
 $(document).ready(function () {
     let $ptty = $('.terminal').Ptty({
         i18n: {
@@ -41,16 +53,21 @@ $(document).ready(function () {
 
 
 //end of the html page
+const colorLetter = element => {
 
+};
 
 
 //our divs and other elements
 
-let header = selector(".intro");
+const header = selector(".intro");
 
-slowPrint(msgg, "HI My Name Is Nick", 100)
+
+slowPrint(msgg, "HI My Name Is Nick", 100);
 
 setTimeout(() => {
 
-slowPrint(header, `I'am A 17 Year Old JavaScript Developer use this terminal to view my projects type in help to start if you are not expirianced with a terminal use a menu`, 30)
+    slowPrint(header, `I'am A 17 Year Old JavaScript Developer use this terminal to view my projects type in help to start if you are not expirianced with a terminal use a menu`, 30)
 }, 4000);
+
+but.addEventListener('click', () => console.log("hello"));
