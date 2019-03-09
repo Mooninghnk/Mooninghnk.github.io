ca5c87f532443764182fd920ac6c66746de62ad2
Mooninghnk <moonnightnk@gmail.com>
2019-03-09 14:33:32 -0500

    ux change

--- js.js
+++ js.js
@@ -14,12 +14,17 @@ addMessage(body, "<center><h3 class='intro'></h3></center>");
 addMessage(body, `<center><button class="xcontent"><h1>👇</h1></button></center>`)
 //addMessage(body, `<a href="#footer">x</a>`)
 addMessage(body, "", "long")
-addMessage(body, `<center><h1 class="projects">MY PROJECTS</h1></center><br>`)
+addMessage(body, `<center><h1 class="projects"></h1></center><br>`)
 addMessage(body, `
 
-<ul id="footer"><center><p>ERC-20 Token Service</p><img src="./lib/met-gif.gif" class="metaWallet"></center></ul>
-
-
+<ul id="footer">
+    <center>
+        <p>Browser Wallet</p><img src="./img-gif/meta-gif.gif" class="metaWallet">
+    </center>
+</ul>
+<div class="meta-text" style = "text-align: center;">
+<p class= "browser-wallet-text" style=" color: white;"></p>
+</div>
 
 `, "othct")
 
@@ -31,6 +36,11 @@ const msgg = selector(".msg");
 const footer = selector("#footer");
 const menu = selector(".menu");
 const xeum_window = selector(".xeum-window")
+const proj = selector(".projects");
+const meta_wallet = selector(".metaWallet");
+const header = selector(".intro");
+const metaTag = selector(".metaWallet");
+const metaText = selector(".browser-wallet-text");
 
 $(document).ready(function () {
     let $ptty = $('.terminal').Ptty({
@@ -66,7 +76,7 @@ $(document).ready(function () {
         method: function (cmd) {
             const xcs  = document.querySelector(".input");
             cmd.out = "running meta";
-            return slowPrint(xcs, `  I'am A 17 Year Old JavaScript Developer use this terminal to view my projects type in help to start if you are not expirianced with a terminal use a menu press enter and clear to clear this text`, 30)
+            return printOut(xcs, `  I'am A 17 Year Old JavaScript Developer use this terminal to view my projects type in help to start if you are not expirianced with a terminal use a menu press enter and clear to clear this text`, 0, 30)
         },
         options: [],
         help: 'runes xeum'
@@ -82,17 +92,15 @@ $(document).ready(function () {
 
 //our divs and other elements
 
-const header = selector(".intro");
-const metaTag = selector(".metaWallet");
 
-slowPrint(msgg, "HI My Name Is Nick", 100);
+
+printOut(msgg, "HI My Name Is Nick", 0, 100);
 
 
 setTimeout(() => {
     const xcs  = document.querySelector(".input");
-    console.log(xcs)
-   slowPrint(xcs, `type in hello to view my into :)`, 40)
-}, 4000);
+    printOut(xcs, `type in hello to view my into :)`, 0, 40)
+}, 2000);
 
 
 but.addEventListener('click', () => {
@@ -111,3 +119,12 @@ menu.addEventListener("click", () => {
       }
     //element.classList.remove("xeum-window")
 });
+printOut(proj, "MY PROJECTS: {",0, 100 );
+
+meta_wallet.addEventListener("mouseover", () => {
+    if(metaText.innerHTML.length <= 0) {
+    printOut(metaText, "This is one of my first projects, this wallet service is using web3 api for its functionality it supports all sorts of transfers", 0, 50)
+    }
+    else printOut(metaText, "", 0, 100)
+})
+
